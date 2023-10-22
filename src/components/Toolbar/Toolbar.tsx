656f1a78e0360af94ea4ffadc6bc15252824a533
Caleb Zearing <czearing@outlook.com>
2023-10-21 18:29:56 -0700

    Adding mobile support to taskbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Toolbar as ToolbarComponent, ToolbarButton } from '@fluentui/react-components';
+import {
+  Toolbar as ToolbarComponent,
+  ToolbarButton,
+  Overflow,
+  OverflowItem,
+  MenuTrigger,
+  MenuPopover,
+  MenuList,
+  MenuItem,
+  Menu,
+  tokens,
+  useIsOverflowItemVisible,
+  useOverflowMenu,
+} from '@fluentui/react-components';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { makeStyles } from '@griffel/react';
-import { tokens } from '@fluentui/react-theme';
+import { Navigation24Regular } from '@fluentui/react-icons';
 
 const useToolbarStyles = makeStyles({
   root: {
@@ -17,11 +31,12 @@ const useToolbarStyles = makeStyles({
     boxSizing: 'border-box',
     flexShrink: 0,
     zIndex: 10000,
-    boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.25)',
+    boxShadow: tokens.shadow4,
   },
 
   toolbarButton: {
     height: '48px',
+    minWidth: '80px',
   },
   flexGrow: {
     flexGrow: 1,
@@ -30,32 +45,96 @@ const useToolbarStyles = makeStyles({
     textDecorationLine: 'none',
     color: tokens.colorNeutralForeground1,
   },
+  toolbarButtonContainer: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'flex-end',
+    minWidth: 0,
+    width: '100%',
+    alignItems: 'end',
+  },
 });
 
-export const Toolbar = () => {
+const OverflowMenuItem: React.FC<{ id: string; href: string }> = props => {
+  const { id, href } = props;
+  const isVisible = useIsOverflowItemVisible(id);
+  const router = useRouter();
+
+  if (isVisible) {
+    return null;
+  }
+
+  const handleClick = () => {
+    router.push(href);
+  };
+
+  return <MenuItem onClick={handleClick}>{id}</MenuItem>;
+};
+
+const OverflowMenu: React.FC<{ items: Array<{ content: string; href: string }> }> = ({ items }) => {
+  const { ref, overflowCount, isOverflowing } = useOverflowMenu<HTMLButtonElement>();
+
   const toolbarStyles = useToolbarStyles();
+
+  if (!isOverflowing) {
+    return null;
+  }
+
+  return (
+    <Menu>
+      <MenuTrigger disableButtonEnhancement>
+        <ToolbarButton
+          aria-label="More"
+          icon={<Navigation24Regular />}
+          ref={ref}
+          className={toolbarStyles.toolbarButton}
+        />
+      </MenuTrigger>
+      <MenuPopover>
+        <MenuList>
+          {items.map(item => {
+            return <OverflowMenuItem key={item.content} id={item.content} href={item.href} />;
+          })}
+        </MenuList>
+      </MenuPopover>
+    </Menu>
+  );
+};
+const toolbarItems = [
+  { content: 'About', href: '/about' },
+  { content: 'Music', href: '/music' },
+  { content: 'Contact', href: '/contact' },
+  { content: 'Live', href: '/live' },
+  { content: 'Shop', href: '/shop' },
+];
+
+export const Toolbar = () => {
+  const router = useRouter();
+  const toolbarStyles = useToolbarStyles();
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
 
   return (
     <ToolbarComponent className={toolbarStyles.root}>
       <Link href="/" passHref>
-        <h1>Nyx</h1>
+        <ToolbarButton className={toolbarStyles.toolbarButton}>
+          <h1>Nýx</h1>
+        </ToolbarButton>
       </Link>
-      <div className={toolbarStyles.flexGrow} />
-      <Link href="/about" passHref>
-        <ToolbarButton className={toolbarStyles.toolbarButton}>About</ToolbarButton>
-      </Link>
-      <Link href="/music" passHref>
-        <ToolbarButton className={toolbarStyles.toolbarButton}>Music</ToolbarButton>
-      </Link>
-      <Link href="/contact" passHref>
-        <ToolbarButton className={toolbarStyles.toolbarButton}>Contact</ToolbarButton>
-      </Link>
-      <Link href="/live" passHref>
-        <ToolbarButton className={toolbarStyles.toolbarButton}>Live</ToolbarButton>
-      </Link>
-      <Link href="/shop" passHref>
-        <ToolbarButton className={toolbarStyles.toolbarButton}>Shop</ToolbarButton>
-      </Link>
+      <Overflow>
+        <div className={toolbarStyles.toolbarButtonContainer}>
+          {toolbarItems.map((item, index) => (
+            <OverflowItem key={index} id={item.content}>
+              <ToolbarButton className={toolbarStyles.toolbarButton} onClick={() => handleNavigation(item.href)}>
+                {item.content}
+              </ToolbarButton>
+            </OverflowItem>
+          ))}
+          <OverflowMenu items={toolbarItems} />
+        </div>
+      </Overflow>
     </ToolbarComponent>
   );
 };
