3b0c940c650b8f328f27d3f6639c45c9a57a7f0e
czearing <czearing@outlook.com>
2023-10-26 17:41:47 -0700

    Meh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { createDOMRenderer, renderToStyleElements } from '@fluentui/react-components';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
